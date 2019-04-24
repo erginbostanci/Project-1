0e61ca59f694ceae2df96e18f0e234e86c2bd681
erginbostanci <erginbostanci@gmail.com>
2019-04-24 09:16:11 -0500

    Project-1

---
+++ erginmadethis/hello.py
@@ -0,0 +1,24 @@
+Project Title: Higher Educational Analysis by University/College
+
+•	Team Members: Brandon Van Nice,Ergin Bostanci, Michael Chopade
+
+•	Project Description: Higher Analysis of Education Data
+
+•	Research Questions to Answers
+1.	Accreditaion of Schools
+2.	Demographic Breakdown
+3.	SocioEconomic level of students
+4.	GrantSchool awarded
+5.	Scholarship chance by ethinicity
+6.	Admissinon test Scores at institution ie ACT,SAT,GRE
+7.	Private,Public
+8.	Urban/Rural/City
+9.	Online/Campus
+10.	School Budgets
+11.	Graduation Rates
+12.	Enrolment Numbers
+•	Dataset to be used: Gov Eduation Data data.gov, US Census Data
+
+•	Rough Breakdown of Tasks : Make it user friendly and understandable, Collect the data, clean the data, analyze the data, Plot and graph, make a conclusion to find the best fits.
+
+
